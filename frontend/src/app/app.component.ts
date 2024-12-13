17cc7a2b5746cd34ae691fcad6eaca14fd97475f
Domepo <dominiktb2@gmail.com>
2024-12-13 11:02:55 +0100

    first site

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { GameHomescreenComponent } from './pages/game-homescreen/game-homescreen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgbAccordionModule],
-  template: `
-    <div ngbAccordion>
-      <div ngbAccordionItem>
-        <h2 ngbAccordionHeader>
-          <button ngbAccordionButton>Simple</button>
-        </h2>
-        <div ngbAccordionCollapse>
-          <div ngbAccordionBody>
-            Accordion Body Content
-          </div>
-        </div>
-      </div>
-    </div>
-  `,
+  imports: [NgbAccordionModule,GameHomescreenComponent],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GameHomescreenComponent } from './pages/game-homescreen/game-homescreen.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'game-homescreen', component: GameHomescreenComponent },
+
+];
