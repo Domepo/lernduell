5eeeef4235f3fc5d1c5f970e456d5c7b7984b772
Domepo <dominiktb2@gmail.com>
2024-12-19 09:26:54 +0100

    fastapi integration

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,17 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  configData: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getConfig('http://127.0.0.1:8000/').subscribe(data => {
+      this.configData = data;
+      console.log(this.configData);
+      return this.configData;
+    });
+  }
+}
+
