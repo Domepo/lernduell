a945f7bbdea7a4b11da209c3d1a787ca80a0f07f
Domepo <dominiktb2@gmail.com>
2024-12-28 11:09:12 +0100

    socket io (not ready)

---
+++ frontend/src/app/socket.service.ts
@@ -0,0 +1,33 @@
+// src/app/services/socket.service.ts
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  private socket: Socket;
+
+  constructor() {
+    this.socket = io('http://localhost:3000/');
+  }
+
+  emit(event: string, data: any) {
+    console.log(data);
+    this.socket.emit(event, data);
+  }
+
+  on(event: string): Observable<any> {
+    return new Observable((observer) => {
+      this.socket.on(event, (data) => {
+        observer.next(data);
+      });
+
+      // Handle cleanup
+      return () => {
+        this.socket.off(event);
+      };
+    });
+  }
+}
