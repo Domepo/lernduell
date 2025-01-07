ad0b60ce31052cb0254459873af5873781c3ad04
Domepo <dominiktb2@gmail.com>
2025-01-07 18:44:51 +0100

    websocket connection, deactivate ssr

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,28 @@
 // src/app/app.component.ts
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from './socket.service';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], // Korrektur: styleUrls im Plural
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+
 })
 export class AppComponent implements OnInit {
-  currentTime: string = ''; // Property für die Uhrzeit
 
-  constructor(private webSocketService: SocketService) {}
+  title= "test";
+  currentTime = ''; // Property für die Uhrzeit
+
+
+  constructor(private socketService: SocketService) {}
 
   ngOnInit() {
-    this.webSocketService.listen('time').subscribe((data: any) => {
-      this.currentTime = data;
-      console.log(this.currentTime);
+    this.socketService.listen('time').subscribe((data: any) => {
+      this.currentTime = data.time;
     });
   }
 }
